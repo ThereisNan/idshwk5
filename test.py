733bd2680a75fac5478a30d992eac88a1e0612c4
小魏子 <2048895733qq@sina.com>
2020-05-05 22:17:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,10 @@ class Domain:
 	def __init__(self,_name,_label):
 		self.name = _name
 		self.label = _label
-    
-        def returnData(self):
-		return analyse(self.name)
+
+
+	def returnData(self):
+		return processData(self.name)
 
 	def returnLabel(self):
 		if self.label == "notdga":
@@ -16,12 +17,13 @@ class Domain:
 		else:
 			return 1
 
-def analyse(name):
-	num=0;
+
+def processData(name):
+	numofn=0;
 	for i in name:
 		if i.isdigit():
-			num=num+1;
-	return [len(name),num];
+			numofn=numofn+1;
+	return [len(name),numofn];
 
 
 def initData(filename):
@@ -45,20 +47,20 @@ def main():
 	clf = RandomForestClassifier(random_state=0)
 	clf.fit(featureMatrix,labelList)
 	f=open("test.txt",mode='r');
-	output=open("result.txt",mode='w');
+	foutput=open("result.txt",mode='w');
 	for line in f:
 		line = line.strip()
 		if line.startswith("#") or line =="":
 			continue
 		tokens = line.split(",")
 		name = tokens[0]
-		result=clf.predict([analyse(name)]);
+		result=clf.predict([processData(name)]);
 		if(result ==0):
-			output.write(name+","+"notdga\n")
+			foutput.write(name+","+"notdga\n")
 		else:
-			output.write(name+","+"dga\n")
+			foutput.write(name+","+"dga\n")
 	f.close();
-	output.close();
+	foutput.close();
 
 
 if __name__ == '__main__':
